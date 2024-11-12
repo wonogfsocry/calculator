14edb63c026ce202268da83ec3daec85eab7b70d
wonogfsocry <yangneil930406@gmail.com>
2024-11-12 16:54:40 +0800

    Initial Commit

---
+++ calculator/calculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  calculatorApp.swift
+//  calculator
+//
+//  Created by user10 on 2024/11/12.
+//
+
+import SwiftUI
+
+@main
+struct calculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
